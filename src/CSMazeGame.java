911cbd1e90c606521b77083e009e14a54f56a9a8
Rishik Yechuri <rishikyechuri@protonmail.com>
2020-11-17 09:16:41 -0800

    Base Structure Created

--- src/CSMazeGame.java
+++ src/CSMazeGame.java
@@ -1,3 +1,28 @@
 public class CSMazeGame {
-  // this is a comment added directly in github
+    int[][] maze = new int[20][20];
+
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        long currentTime = System.currentTimeMillis();
+        boolean mazeCompleted = false;
+        initializeMaze();
+        while (!mazeCompleted) {
+            updateMaze();
+            updateFrontEnd();
+            if (/*All options have been explored or Maze has been completed*/
+                    false) {
+                mazeCompleted = true;
+            }
+        }
+    }
+
+    public static void initializeMaze() {
+        System.out.println("Works");
+    }
+
+    public static void updateMaze() {
+    }
+
+    public static void updateFrontEnd() {
+    }
 }
