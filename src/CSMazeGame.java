e5d2e2e3873667857b6c21e592f7ce886c633359
Rishik Yechuri <rishikyechuri@protonmail.com>
2020-11-17 09:50:45 -0800

    Data copied over
    -Maze is copied from text file to 2d array
    -players initial position is set

--- src/CSMazeGame.java
+++ src/CSMazeGame.java
@@ -1,7 +1,10 @@
-public class CSMazeGame {
-    int[][] maze = new int[20][20];
+import java.io.*;
+import java.util.Arrays;
 
-    public static void main(String[] args) {
+public class CSMazeGame {
+    static int[][] maze = new int[20][20];
+
+    public static void main(String[] args) throws IOException {
         long startTime = System.currentTimeMillis();
         long currentTime = System.currentTimeMillis();
         boolean mazeCompleted = false;
@@ -16,8 +19,24 @@ public class CSMazeGame {
         }
     }
 
-    public static void initializeMaze() {
-        System.out.println("Works");
+    //0 represents an empty space,1 represents a wall,and -1 represents the player
+    //Future block like helped blocks or harmful blocks will use the same scheme
+    public static void initializeMaze() throws IOException {
+        File file = new File("Maze1");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+       for(int y=0;y<20;y++){
+           for(int x=0;x<20;x++){
+               maze[y][x] = br.read()-48;
+               if(maze[y][x] == -38 ){
+                   maze[y][x] = br.read()-48;
+               }
+           }
+       }
+       for(int y =0;y<20;y++){
+           if(maze[y][19] == 0){
+               maze[y][19] = -1;
+           }
+       }
     }
 
     public static void updateMaze() {
