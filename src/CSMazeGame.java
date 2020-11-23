89cec76e478c956267563316be5deb5d98699640
Rishik Yechuri <rishikyechuri@protonmail.com>
2020-11-23 12:43:05 -0800

    New Objects Added
    -Helper and danger objects created

--- src/CSMazeGame.java
+++ src/CSMazeGame.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.image.BufferStrategy;
 import java.io.*;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Stack;
 
 public class CSMazeGame {
@@ -11,7 +12,7 @@ public class CSMazeGame {
     static Coord currentMove;
     static Stack visitStack;
     static boolean searchingDone = false;
-
+    HashMap<Coord, Integer> obstacleLocations = new HashMap<Coord, Integer>();
     //Visual side of things being declared
     static JFrame frame;
     static Canvas canvas;
@@ -134,11 +135,54 @@ public class CSMazeGame {
         int a = 0;
         boolean keepLoopRunning = true;
         boolean foundSomething = false;
+        int currentValue = 0;
+        if(inBounds(currentMove.rPos,currentMove.cPos)){
+            currentValue = maze[currentMove.rPos][currentMove.cPos];
+        }
+        if(currentValue > 1){
+            keepLoopRunning = false;
+            if (currentValue > 1 && currentValue < 6) {
+                switch (currentValue) {
+                    case 2:
+                        rMovement = -1;
+                        break;
+                    case 3:
+                        cMovement = -1;
+                        break;
+                    case 4:
+                        rMovement = 1;
+                        break;
+                    case 5:
+                        cMovement = 1;
+                        break;
+                }
+            }
+            if(currentValue > 5 ){
+                switch (currentValue){
+                    case 6:
+                        rMovement = -1;
+                        break;
+                    case 7:
+                        cMovement = -1;
+                        break;
+                    case 8:
+                        rMovement = 1;
+                        break;
+                    case 9:
+                        cMovement = 1;
+                        break;
+                }
+            }
+        }
         int[][] holdPossibleMoves = new int[][]{{-1, 0}, {-1, -1}, {0, -1}, {1, -1}, {1, 0}, {1, 1}, {0, 1}, {-1, 1}};
         while (keepLoopRunning) {
             rMovement = holdPossibleMoves[a][0];
             cMovement = holdPossibleMoves[a][1];
-            if (inBounds(currentMove.rPos + rMovement, currentMove.cPos + cMovement) && maze[currentMove.rPos + rMovement][currentMove.cPos + cMovement] == 0) {
+            int spaceValue = 1;
+            if (inBounds(currentMove.rPos + rMovement, currentMove.cPos + cMovement)) {
+                spaceValue = maze[currentMove.rPos + rMovement][currentMove.cPos + cMovement];
+            }
+            if (inBounds(currentMove.rPos + rMovement, currentMove.cPos + cMovement) && spaceValue != 1 && spaceValue > -1) {
                 currentMove.rPos += rMovement;
                 currentMove.cPos += cMovement;
                 return true;
@@ -169,10 +213,17 @@ public class CSMazeGame {
                     color = new Color(0, 0, 0);
                 } else if (mazeValue == 0) {
                     color = new Color(255, 255, 255);
-                } else if (mazeValue == -1) {
+                } /*else if (mazeValue == -1) {
                     color = new Color(20, 20, 220);
-                } else if (mazeValue == -2) {
+                } */ else if (mazeValue == -2) {
                     color = new Color(190, 150, 30);
+                }else if(mazeValue > 1 && mazeValue < 6){
+                    color = new Color(15,200,20);
+                }else if(mazeValue > 5 ){
+                    color = new Color(200,15,20);
+                }
+                if (y == currentMove.rPos && x == currentMove.cPos) {
+                    color = new Color(20, 20, 220);
                 }
                 g.setColor(color);
                 g.fillRect(x * 50, y * 50, 50, 50);
