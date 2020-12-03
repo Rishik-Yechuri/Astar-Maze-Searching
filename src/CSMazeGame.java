039f57cfeeb4217045073fab9572be6e5292ca17
Rishik Yechuri <rishikyechuri@protonmail.com>
2020-12-02 18:41:06 -0800

    Obstacles and Data storage
    -All the coordinates that have been visited are stored in a text file for all mazes,along with the total number of moves that were made
    -Obstacles generated depending on inputted number(1-8)
    -Obstacles are shown in brown

--- src/CSMazeGame.java
+++ src/CSMazeGame.java
@@ -2,15 +2,15 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferStrategy;
 import java.io.*;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Stack;
+import java.util.*;
 
 public class CSMazeGame {
+    static int randomNumber = 0;
     static Coord previousMove;
     static int[][] maze = new int[20][20];
     static Coord currentMove;
     static Stack visitStack;
+    static Queue<Coord> fullStack;
     static boolean searchingDone = false;
     static int timesToGo = 0;
     static int directionToGo = 0;
@@ -20,12 +20,21 @@ public class CSMazeGame {
     static Canvas canvas;
     static BufferStrategy bufferStrategy;
     static Graphics2D g;
+    //Bufferedreader for Maze data created
+    static File file;
+    static BufferedWriter br;
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
         long startTime;
         long currentTime;
         //boolean mazeCompleted = false;
+        file = new File("AiData");
+        br = new BufferedWriter(new FileWriter(file));
+        //br.flush();
         for(int x=1;x<=3;x++){
+            System.out.print("What is the random number you would like to use(1-8):");
+            randomNumber = scanner.nextInt();
             initializeMaze(x);
             startTime = System.currentTimeMillis();
             while (!searchingDone) {
@@ -40,6 +49,7 @@ public class CSMazeGame {
                     }
                     startTime = currentTime;
                 }
+
             }
             searchingDone = false;
             if(x==3){
@@ -47,7 +57,18 @@ public class CSMazeGame {
                 g.setFont(new Font("TimesRoman", Font.PLAIN, 34));
                 g.drawString("Mazes Completed",350,450);
             }
+            //br.write(String.valueOf(fullStack) + " " + fullStack.size());
+            String entireStack = "";
+            for(int p=0;p<fullStack.size();p++){
+                Coord tempCoord = fullStack.poll();
+                String finalString = "(" + tempCoord.rPos + "," + tempCoord.cPos + ")";
+                entireStack+=finalString;
+            }
+            br.write("# of steps:" + fullStack.size() + " Visited Path:" + entireStack);
+            br.newLine();
+            System.out.println(String.valueOf(entireStack));
         }
+        br.close();
     }
 
     //0 represents an empty space,1 represents a wall,and -1 represents the player
@@ -66,13 +87,48 @@ public class CSMazeGame {
             }
         }
         for (int y = 0; y < 20; y++) {
+            for(int x=0;x<20;x++){
+                if(inBounds(y+1,x+1) && maze[y][x] == 0 && maze[y+1][x] == 0 && maze[y][x+1] == 0 && maze[y+1][x+1] == 0){
+                    if(randomNumber == 1){
+                        maze[y][x] = -3;
+                    }else if(randomNumber == 2){
+                        maze[y+1][x]=-3;
+                    }
+                    else if(randomNumber == 3){
+                        maze[y+1][x+1]=-3;
+                    }
+                    else if(randomNumber == 4){
+                        maze[y][x+1]=-3;
+                    }else{
+                        int whichOne = (int)(Math.random()*2);
+                        ArrayList<Coord> randomPlaces = new ArrayList<>();
+                        if(randomNumber == 5){
+                            randomPlaces.add(new Coord(y,x));
+                            randomPlaces.add(new Coord(y+1,x));
+                        }else if(randomNumber == 6){
+                            randomPlaces.add(new Coord(y+1,x));
+                            randomPlaces.add(new Coord(y+1,x+1));
+                        }else if(randomNumber == 7){
+                            randomPlaces.add(new Coord(y+1,x+1));
+                            randomPlaces.add(new Coord(y,x+1));
+                        }else if(randomNumber == 8){
+                            randomPlaces.add(new Coord(y,x+1));
+                            randomPlaces.add(new Coord(y,x));
+                        }
+                        maze[randomPlaces.get(whichOne).rPos][randomPlaces.get(whichOne).cPos] = -3;
+                    }
+                }
+            }
             if (maze[y][19] == 0) {
                 maze[y][19] = -1;
                 currentMove = new Coord(y, 19);
             }
         }
+        //for(int y =0;y<20;y++){}
         visitStack = new Stack();
+        fullStack = new LinkedList<>();
         visitStack.push(currentMove);
+        //fullStack.add(currentMove);
         //Initialize front end
         if(frame == null) {
             frame = new JFrame("Maze Path Finder");
@@ -112,6 +168,7 @@ public class CSMazeGame {
             // maze[currentMove.rPos][currentMove.cPos] = -2;
             currentMove = new Coord(currentMove.rPos, currentMove.cPos);
             visitStack.push(new Coord(currentMove.rPos,currentMove.cPos));
+            fullStack.add(new Coord(currentMove.rPos,currentMove.cPos));
         }
         maze[oldCoord.rPos][oldCoord.cPos] = -2;
         //maze[currentMove.rPos][currentMove.cPos] = -1;
@@ -138,12 +195,12 @@ public class CSMazeGame {
                 keepGoingBack = false;
             }
             updateFrontEnd();
-            Thread.sleep(950);
+            Thread.sleep(5/*950*/);
         }
         /*if (!visitStack.isEmpty()) {
             currentMove = (Coord) visitStack.peek();
         }*/
-        Thread.sleep(950);
+        Thread.sleep(5/*950*/);
         updateFrontEnd();
     }
 
@@ -259,6 +316,8 @@ public class CSMazeGame {
                     color = new Color(15, 200, 20);
                 } else if (mazeValue > 5) {
                     color = new Color(200, 15, 20);
+                }else if(mazeValue == -3){
+                    color = new Color(102,102,51);
                 }
                 if (y == currentMove.rPos && x == currentMove.cPos) {
                     color = new Color(20, 20, 220);
